7c51bc14add10270e245485113a8ffe55a899913
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-27 21:48:19 +0200

    added the id path variable

--- src/main/java/demo/StudentController.java
+++ src/main/java/demo/StudentController.java
@@ -1,30 +1,27 @@
 package demo;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
 
-    @Autowired
-    private ServiceClass serviceClass;
+	@Autowired
+	private ServiceClass serviceClass;
 
-    @PostMapping("/save")
-    public StudentClass SaveStudent(@RequestBody StudentClass studentClass){
-        return serviceClass.saveStudent(studentClass);
+	@PostMapping("/save")
+	public StudentClass SaveStudent(@RequestBody StudentClass studentClass) {
+		return serviceClass.saveStudent(studentClass);
 
-    }
-    @GetMapping("/getStudent")
-    public StudentClass getStudents(@PathVariable("id") Long id){
-        return serviceClass.getStudents(id);
-    }
+	}
 
-
-
-
-
+	@GetMapping("/getStudent/{id}")
+	public StudentClass getStudents(@PathVariable("id") Long id) {
+		return serviceClass.getStudents(id);
+	}
 
 }
