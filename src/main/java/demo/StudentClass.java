87de644da1210e7ece6c76afae80742413a323a3
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-27 21:47:59 +0200

    removed the ID fro constructor params

--- src/main/java/demo/StudentClass.java
+++ src/main/java/demo/StudentClass.java
@@ -1,63 +1,53 @@
 package demo;
 
-import jakarta.persistence.*;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class StudentClass {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String name;
-    private String department;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String department;
 
-    public StudentClass(Long id, String name, String department) {
-        this.id = id;
-        this.name = name;
-        this.department = department;
-    }
+	public StudentClass(String name, String department) {
+		this.name = name;
+		this.department = department;
+	}
 
-    public StudentClass() {
-    }
+	public StudentClass() {
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getDepartment() {
-        return department;
-    }
+	public String getDepartment() {
+		return department;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setDepartment(String department) {
-        this.department = department;
-    }
+	public void setDepartment(String department) {
+		this.department = department;
+	}
 
-    @Override
-    public String toString() {
-        return "StudentClass{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", department='" + department + '\'' +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "StudentClass{" + "id=" + id + ", name='" + name + '\'' + ", department='" + department + '\'' + '}';
+	}
 }
-
-
-
-
-
